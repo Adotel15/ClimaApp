a8b8b6e53e2a54f6f17b1ac84cda21da822e911c
Adotel <adriandotel@gmail.com>
2022-08-09 13:00:49 +0200

    Consultando la API

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -5,6 +5,7 @@ const ClimaContext = createContext()
 
 const ClimaProvider = ({children}) => {
 
+    const [resultado, setResultado] = useState({})
     const [busqueda, setBusqueda] = useState({
         ciudad: '',
         pais: ''
@@ -17,8 +18,27 @@ const ClimaProvider = ({children}) => {
         })
     }
 
-    const consultarClima = datos => {
-        console.log(datos)
+    const consultarClima = async datos => {
+        try {
+
+            const { ciudad, pais } = datos
+            const API_KEY = import.meta.env.VITE_API_KEY;
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${API_KEY}`
+            
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+
+            const {lat, lon} = resultado[0];
+
+            const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+            const respuestaClima = await fetch(urlClima);
+            const resultadoClima = await respuestaClima.json();
+
+            setResultado(resultadoClima)
+
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     return (
@@ -26,7 +46,8 @@ const ClimaProvider = ({children}) => {
             value = {{
                 busqueda,
                 datosBusqueda,
-                consultarClima
+                consultarClima,
+                resultado
             }}
         >
             {children}
